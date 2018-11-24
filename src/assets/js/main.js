335752fea27f528f98fec251be3076a5e4e3e686
Myzhe <maxyvon@gmx.de>
2018-11-24 21:30:37 +0100

    Add basic audioAPI js classes

---
+++ src/assets/js/main.js
@@ -0,0 +1,39 @@
+
+// Init code
+if (!navigator.getUserMedia) {
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+  }
+
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+
+
+// Audio and Sound code
+class AudioOutput {
+
+    constructor() {
+        this.context = new AudioContext();
+    }
+
+    setInput(input) {
+        input.output.connect(this.context.destination);
+    }
+}
+
+class SoundGenerator {
+
+    constructor(output) {
+        this.output = output.context.createOscillator();
+        this.output.start();
+    }
+
+    setFrequency(f) {
+        this.output.frequency.setValueAtTime(f, output.context.currentTime); 
+    }
+}
+
+// Testing
+var output = new AudioOutput();
+var input = new SoundGenerator(output);
+
+output.setInput(input);
