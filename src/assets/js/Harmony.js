5b7d5d84e11144803841ef2014faf0299619ff6d
Myzhe <maxyvon@gmx.de>
2018-11-25 05:06:06 +0100

    Finally!

--- src/assets/js/Harmony.js
+++ src/assets/js/Harmony.js
@@ -16,15 +16,15 @@ class Harmony {
 
     //Keinen Bock auf Kommentierung sorry
     toneNumGen() {
-        if (ball1.x < 300); {
+        if (ball1.x < 300) {
             let nums = [0, 2, 4, 7, 9, 12, -2, -4, -7, -9, -12];
-        };
-        else if (300 <= ball1.x < 500); {
+        }
+        else if (300 <= ball1.x < 500) {
             let nums = [0, 2, 4, 5, 7, 9, 11, 12, -2, -4, -5, -7, -9, -11, -12];
-        };
-        else if (500 <= ball1.x); {
+        }
+        else if (500 <= ball1.x) {
             let nums = [0, 2, 3, 5, 7, 8, 10, 12, -2, -3, -5, -7, -8, -10, -12];
-        };
+        }
         let rnum = nums[Math.floor(Math.random() * nums.length)];
         return nums[rnum];
     };
